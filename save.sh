7fd0a4f98299b65a9dc25916b04a07b5493ee717
julienMoran <julien.moran@gmail.com>
2013-01-09 09:28:40 +0100

    Changements de julien

--- save.sh
+++ save.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git config --global user.name "julienMoran"
+git config --global user.email "julien.moran@gmail.com"
 git config remote.origin.url https://yannickkuhn:polytech06@github.com/yannickkuhn/automate.git
 git add *
 git commit -a -m "Changements de julien"
