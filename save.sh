51bb8547f1b4d3a22b39ca19292fd9282da34213
yannickkuhn06@gmail.com <yannickkuhn@yannickkuhn-PC.sictiam.local>
2013-01-09 09:25:57 +0100

    Changements de julien

--- save.sh
+++ save.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git config remote.origin.url https://stagiaireautomate:stagiaire06@github.com/yannickkuhn/automate.git
+git config remote.origin.url https://yannickkuhn:polytech06@github.com/yannickkuhn/automate.git
 git add *
 git commit -a -m "Changements de julien"
 git push origin master
